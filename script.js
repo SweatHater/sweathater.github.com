cf70312f4e182205e3bc9763e6e23f34fbbc592a
SweatHater <tahailyas821@gmail.com>
2026-03-07 00:07:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.getElementById("btn").addEventListener("click", function() {
+document.getElementById("btn").addEventListener("click", function(){
+
 alert("Thanks for visiting my website!");
+
 });
